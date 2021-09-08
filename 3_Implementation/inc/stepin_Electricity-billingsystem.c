8f5ef792d1f818c69a995346572d707cc1a2ebbb
BattuPravalika <89590962+BattuPravalika@users.noreply.github.com>
2021-09-08 22:06:54 +0530

    Create stepin_Electricity-billingsystem.c

---
+++ 3_Implementation/inc/stepin_Electricity-billingsystem.c
@@ -0,0 +1,23 @@
+/**
+ * @file stepin_Electricity-billingsystem.h
+ * @author Battu Pravalika
+ * @brief files for calculating electric bill based on different regions
+ * 
+ * 
+ */
+#ifndef __ELECTRICITY_BILL_H__
+#define __ELECTRICITY_BILL_H__
+#include<stdio.h>
+#include<conio.h>
+#include<math.h>
+/**
+ * @brief different regions
+ * 
+ */
+ * 
+ */
+float southern(float units);
+ float northern(float units);
+ float northeast(float units);
+ float centralgrid(float units);
+ #endif
